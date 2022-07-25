d277237aefa627b672eac5c56ecfcb012a9f466a
Albin CR <albincr@Albins-MacBook-Pro.local>
2022-07-25 23:55:34 +0530

    Deploy pp $ tcc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "//autobots.co.in/precache-manifest.2b8a3f3d1443be21fd1f5f70fe19a95d.js"
+  "/precache-manifest.b3d8d5772ae5ed978378f7138310bb0d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//autobots.co.in/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
